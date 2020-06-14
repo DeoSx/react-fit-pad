dc4f2a197ad4d1abc3b9010bfa6fae09aa70a6ea
Damir <damir@rocketfirm.com>
2020-06-14 22:01:06 +0600

    feat: add modal & input components

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Input from '../UI/Input/Input'
 
-const Modal = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import './Modal.scss'
+
+class Modal extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: ''
+  }
+
+  changeHandler = (value, field) => {
+    this.setState({
+      [field]: value
+    })
+  }
+
+  render() {
+    return (
+      <div className="overlay">
+        <div className="modal main-ui">
+          <Input
+            type="text"
+            label="Name"
+            onChange={(event) => this.changeHandler(event.target.value, 'name')}
+          />
+          <Input
+            type="text"
+            label="Email"
+            onChange={(event) => this.changeHandler(event.target.value, 'name')}
+          />
+          <Input
+            type="text"
+            label="Password"
+            onChange={(event) => this.changeHandler(event.target.value, 'name')}
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Modal
---
+++ client/src/components/UI/Input/Input.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Input.scss'
+
+const Input = (props) => {
+  const inputType = props.type || 'text'
+  const htmlFor = `${inputType}-${Math.random()}`
+  return (
+    <div className="input-group">
+      <label htmlFor={htmlFor}>{props.label}</label>
+      <input type={inputType} id={htmlFor} onChange={props.onChange} />
+    </div>
+  )
+}
+
+export default Input
