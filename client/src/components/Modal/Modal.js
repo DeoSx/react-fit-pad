46e43f21cc881039d3449175f8f575c132ba6475
Damir <damir@rocketfirm.com>
2020-06-17 17:45:51 +0600

    feat: refactor modal component

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,56 +1,15 @@
 import React, { Component } from 'react'
 import axios from '../../axios/'
-import Input from '../UI/Input/Input'
-import Button from '../UI/Button/Button'
 
 import './Modal.scss'
 
 class Modal extends Component {
-  state = {
-    username: '',
-    email: '',
-    password: ''
-  }
-
-  changeHandler = (value, field) => {
-    this.setState({
-      [field]: value
-    })
-  }
-
-  submitHandler = async (data) => {
-    console.log(data)
-    const res = await axios.post('user/signup', data)
-    console.log(res)
-  }
 
   render() {
     return (
       <div className="overlay">
         <div className="modal main-ui">
-          <Input
-            type="text"
-            label="Name"
-            onChange={(event) => this.changeHandler(event.target.value, 'username')}
-          />
-          <Input
-            type="text"
-            label="Email"
-            onChange={(event) =>
-              this.changeHandler(event.target.value, 'email')
-            }
-          />
-          <Input
-            type="password"
-            label="Password"
-            onChange={(event) =>
-              this.changeHandler(event.target.value, 'password')
-            }
-          />
-          <Button
-            text="Registration"
-            onClick={() => this.submitHandler(this.state)}
-          />
+          { this.props.children }
         </div>
       </div>
     )
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,25 +1,74 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from '../../axios'
 import { NavLink } from 'react-router-dom'
+
+import Input from '../UI/Input/Input'
+import Button from '../UI/Button/Button'
+import Modal from '../Modal/Modal'
 import './Navigation.scss'
 
 import { Wrapper } from '../../styles'
 
-const Navigation = () => {
-  return (
-    <Wrapper>
-      <nav className="nav main-ui">
-        <div className="logo">
-          <img src="images/logo.png" />
-        </div>
-        <div className="links">
-          <NavLink exact to="/">
-            Home
-          </NavLink>
-          <NavLink to="/history">History</NavLink>
-        </div>
-      </nav>
-    </Wrapper>
-  )
+class Navigation extends Component {
+  state = {
+    username: '',
+    password: '',
+    email: '',
+    modalState: false
+  }
+
+  changeHandler = (value, field) => {
+    this.setState({
+      [field]: value
+    })
+  }
+
+  render() {
+    const modal = (
+      <Modal>
+        <Input
+          type="text"
+          label="Name"
+          onChange={(event) =>
+            this.changeHandler(event.target.value, 'username')
+          }
+        />
+        <Input
+          type="text"
+          label="Email"
+          onChange={(event) => this.changeHandler(event.target.value, 'email')}
+        />
+        <Input
+          type="password"
+          label="Password"
+          onChange={(event) =>
+            this.changeHandler(event.target.value, 'password')
+          }
+        />
+        <Button text="sign up" onClick={() => this.submitHandler(this.state)} />
+      </Modal>
+    )
+
+    return (
+      <Wrapper>
+        <nav className="nav main-ui">
+          <div className="logo">
+            <img src="images/logo.png" alt="logo" />
+          </div>
+          <div className="links">
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/history">History</NavLink>
+          </div>
+          <div className="auth-block">
+            <a href>Sign in</a>/<a href>Sign up</a>
+          </div>
+        </nav>
+        { this.state.modalState ? modal : null }
+      </Wrapper>
+    )
+  }
 }
 
 export default Navigation
