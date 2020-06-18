0efa7afa27a70052803f7ec363fd4296271c04e2
Damir <damir@rocketfirm.com>
2020-06-18 21:34:41 +0600

    feat: add cross-env

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const config = require('config')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
@@ -16,6 +17,14 @@ app.use(bodyParser.json())
 
 // Routes
 app.use('/api/user', require('./routes/user'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.resolve(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__diname, 'client', 'build', 'index.html'))
+  })
+}
 
 async function start() {
   try {
