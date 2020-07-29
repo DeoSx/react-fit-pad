f5a3926630fe66d137e4047b30e1c77e9c623970
Damir <damir@rocketfirm.com>
2020-07-29 22:25:34 +0600

    feat: create excercise handler and get all(not done)

--- client/src/pages/Excercises/Excercises.js
+++ client/src/pages/Excercises/Excercises.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
+import { createExcercise, getAll } from '../../store/excercise/excercise.api'
 
 import { FormWrapper } from '../../styles'
 import Select from '../../components/UI/Select/Select'
@@ -9,30 +10,43 @@ import Button from '../../components/UI/Button/Button'
 const mockData = [
   {
     name: 'Грудь',
-    idOfTypeMuscle: '1'
+    idOfMuscleType: '1'
   },
   {
     name: 'Ноги',
-    idOfTypeMuscle: '2'
+    idOfMuscleType: '2'
   },
   {
     name: 'Спина',
-    idOfTypeMuscle: '3'
+    idOfMuscleType: '3'
   },
   {
     name: 'Руки',
-    idOfTypeMuscle: '4'
+    idOfMuscleType: '4'
   },
   {
     name: 'Плечи',
-    idOfTypeMuscle: '5'
+    idOfMuscleType: '5'
   }
 ]
 
-const Excercises = () => {
+const Excercises = (props) => {
+  const [selected, setSelected] = useState(null)
+  const [input, setInput] = useState(null)
+
+  useEffect(() => {
+    props.getAllExc()
+    console.log(props)
+  },)
+
   const submitHandler = (e) => {
     e.preventDefault()
-    console.log('submit')
+
+    const data = {
+      name: input,
+      idOfMuscleType: selected.idOfMuscleType
+    }
+    props.createExcercise(data)
   }
 
   return (
@@ -40,14 +54,32 @@ const Excercises = () => {
       <h1>Create</h1>
 
       <FormWrapper onSubmit={(e) => submitHandler(e)}>
-        <Select title="Выбрать мышечную группу" items={mockData} />
-        <Input label="Название упражнения" />
+        <Select
+          title="Выбрать мышечную группу"
+          items={mockData}
+          handler={setSelected}
+        />
+        <Input
+          label="Название упражнения"
+          onChange={(e) => setInput(e.target.value)}
+        />
         <Button text={'Создать'} styleType={'primary'} />
       </FormWrapper>
     </section>
   )
 }
 
-const mapDispatchToProps = () => {}
+const mapStateToProps = (state) => {
+  return {
+    excercise: state.excercise
+  }
+}
 
-export default connect(null, mapDispatchToProps)(Excercises)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createExcercise: (data) => dispatch(createExcercise(data)),
+    getAllExc: () => dispatch(getAll)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Excercises)
--- client/src/store/excercise/excercise.api.js
+++ client/src/store/excercise/excercise.api.js
@@ -1,9 +1,23 @@
 import axios from '../../axios'
+import { getAllAction } from './excercise.actions'
 
 export function createExcercise(data) {
-  return async (dispatch) => {
+  return async () => {
     try {
-      const res = await axios.post('excercise/create', data)
+      await axios.post('excercise/create', data)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+
+export function getAll() {
+  return async dispatch => {
+    try {
+      console.log('before')
+      const res = await axios.get('excercise')
+      await dispatch(getAllAction(res))
+      console.log(res)
     } catch (e) {
       console.error(e)
     }
