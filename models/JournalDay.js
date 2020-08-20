54591a154cc640a183dbba1c586f4c9081b1a29f
Damir <damir@rocketfirm.com>
2020-08-20 21:09:59 +0600

    feat: change model in journal day (not done)

--- models/JournalDay.js
+++ models/JournalDay.js
@@ -1,10 +1,34 @@
 const { Schema, model } = require('mongoose')
 
-const JournalDay = new Schema({
-  day: {
-    type: Array,
-    required: true
+const subCounter = new Schema({
+  reps: {
+    type: String,
+    required: false
+  },
+  weight: {
+    type: String,
+    required: false
   }
+})
+
+const subDay = new Schema({
+  counter: [subCounter],
+  idOfMuscleType: {
+    type: Number
+  },
+  name: {
+    type: String
+  },
+  nameOfMuscleType: {
+    type: String
+  },
+  _id: {
+    type: String
+  }
+})
+
+const JournalDay = new Schema({
+  day: [subDay]
 })
 
 module.exports = model('JournalDay', JournalDay)
