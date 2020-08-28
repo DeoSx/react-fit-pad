eee3b2eddea867a75042ec37f4a06ff006d03374
DeoSx <deosxz123@gmail.com>
2020-08-28 22:18:57 +0600

    feat: buttons state

--- client/src/pages/Journal/Days/Day.js
+++ client/src/pages/Journal/Days/Day.js
@@ -1,24 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { CSSTransition } from 'react-transition-group'
 
 import Button from '../../../components/UI/Button/Button'
 import DayBodyItem from './DaybodyItem'
 
 const Day = (props) => {
   const [change, setChange] = useState(false)
+  const [show, setShow] = useState(false)
 
   const { item } = props
+
+  const buttonsBlock = (
+    <div className="day-buttons">
+      <Button icon="add" float={true} styleType="blue" />
+      <Button styleType="green" small={true} text="Сохранить" />
+    </div>
+  )
+
   return (
     <div className="day-container">
       <div className="day-top">
         <p className="day-date">{item.createdAt.toLocaleString()}</p>
-        <Button styleType="blue" text="Изменить" small={true} />
+
+        {show ? (
+          <Button
+            styleType="red"
+            text="Отмена"
+            small={true}
+            onClick={() => setShow(false)}
+          />
+        ) : (
+          <Button
+            styleType="blue"
+            text="Изменить"
+            small={true}
+            onClick={() => setShow(true)}
+          />
+        )}
       </div>
       <div className="day-body">
         {item.day.map((i) => (
-          <DayBodyItem key={i._id} item={i} />
+          <DayBodyItem key={i._id} item={i} state={show} />
         ))}
       </div>
-      <Button icon="add" float={true} styleType="blue" />
+      <CSSTransition in={show} timeout={300} classNames="fade">
+        {show ? buttonsBlock : <p style={{ display: 'none' }}></p>}
+      </CSSTransition>
 
       {/* <CSSTransition in={modalState} timeout={300} classNames="fade">
         <Modal modalState={modalState} close={closeModalHandler}>
